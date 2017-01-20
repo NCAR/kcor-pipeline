5b291b5d919792e0e1d47ed0a081831d1b4aaa48
kolinski <kolinski@ucar.edu>
2017-01-20 11:36:02 -0700

    Edited fields of kcor_eng_create_table.pl; Created new script, kcor_sgs_create_table.pl.

--- bin/kcor_eng_create_table.pl
+++ bin/kcor_eng_create_table.pl
@@ -8,16 +8,44 @@ use DBI;
 # Create MLSO db table: kcor_eng (mysql).
 # ------------------------------------------------------------------------------
 # Andrew Stanger   MLSO/HAO/NCAR   08 Dec 2015
+# New edits by Don Kolinski Jan 2017
+#	Added new argument containing path/configfile:
+#		config file format:
+#		username = <value>
+#		password = <value>
+#		host = <value>
+#		dbname = <value>
+#	Added/edited database fields
 # ------------------------------------------------------------------------------
-#--- DB name
-#--- DB host
-#--- DB user
-#--- DB password
 
-$db   = "MLSO" ;
-$host = "databases.hao.ucar.edu" ;
-$user = "stanger" ;
-$pass = "mml4so14" ;
+# Check the arguments for existence of config file
+if ($#ARGV != 0 ) {
+    print "Usage: $0 <ConfigFile>\n";
+    exit;
+}
+
+# Warn user of database drop
+print "WARNING!!!! This script will drop the table kcor_eng!\nDo you wish to continue? ";
+print "Press <Enter> to continue, or 'q' to quit: ";
+my $input = <STDIN>;
+exit if $input eq "q\n";
+
+# Read config file
+$configfile = $ARGV[0];
+open (CONFIG, "$configfile") or die "ERROR: Config file not found : $configfile";
+while (<CONFIG>) {
+    chomp;                  # no newline
+    s/#.*//;                # no comments
+    s/^\s+//;               # no leading white
+    s/\s+$//;               # no trailing white
+    next unless length;     # anything left?
+    my ($var, $value) = split(/\s*=\s*/, $_, 2);
+    $configvar{$var} = $value;
+} 
+$user = $configvar{"username"};
+$pass = $configvar{"password"};
+$host = $configvar{"host"};
+$db = $configvar{"dbname"};
 
 #---------------------
 # Connect to database.
@@ -39,7 +67,7 @@ else
 # Create new kcor_eng table.
 #-------------------------------
 
-$command = "DROP TABLE kcor_eng IF EXISTS" ;
+$command = "DROP TABLE IF EXISTS kcor_eng_test" ;
 $sth     = $dbh->prepare ($command) ;
 
 $sth->execute () ;
@@ -50,27 +78,37 @@ if (! $sth)
   die () ;
   }
 
-$command = "CREATE TABLE kcor_eng 
+# Define fields
+#	Notes:
+#	Removed sgs fields and moved them to new table, kcor_sgs
+$command = "CREATE TABLE kcor_eng_test
   (
-  eng_id    INT (10) AUTO_INCREMENT PRIMARY KEY,
-  file_name VARCHAR (32) NOT NULL,
-  date      DATETIME NOT NULL,
-  rcamfocs  FLOAT (6, 2),
-  tcamfocs  FLOAT (6, 2),
-  modltrt   FLOAT (5, 1),
-  o1focs    FLOAT (8, 3),
-  sgsdimv   FLOAT (7, 4),
-  sgsdims   FLOAT (8, 5),
-  sgssumv   FLOAT (7, 4),
-  sgsrav    FLOAT (9, 6),
-  sgsras    FLOAT (9, 6),
-  sgsrazr   FLOAT (5, 1),
-  sgsdecv   FLOAT (10, 7),
-  sgsdecs   FLOAT (10, 7),
-  sgsdeczr  FLOAT (5, 1),
-  sgsscint  FLOAT (7, 3),
-  sgssums   FLOAT (8, 5)
-  )" ;
+  eng_id		INT (10) AUTO_INCREMENT PRIMARY KEY,
+  file_name		CHAR (32) NOT NULL,
+  date_obs		DATETIME NOT NULL,
+  rcamfocs		FLOAT (6, 2),
+  tcamfocs		FLOAT (6, 2),
+  modltrt		FLOAT (6, 2),
+  o1focs		FLOAT (8, 3),
+  level			FLOAT (5, 1),
+  bunit			VARCHAR (15),
+  bzero			INT (10),
+  bscale		FLOAT (5, 4),
+  rcamxcen		FLOAT(9, 3),
+  rcamycen		FLOAT(9, 3),
+  tcamxcen		FLOAT(9, 3),
+  tcamycen		FLOAT(9, 3),
+  rcam_rad		FLOAT(9, 3),
+  tcam_rad		FLOAT(9, 3),
+  mean_phase1	FLOAT (7, 4),
+  cover			CHAR (3),
+  darkshut		CHAR (3),
+  diffuser		CHAR (3),
+  calpol		CHAR (3),
+  calpang		float (8, 3),
+  datatype		VARCHAR (12)
+  
+  )" ;  # TODO: remove _test when in production
 
 $sth = $dbh->prepare ($command) ;
 $sth->execute () ;
---
+++ bin/kcor_sgs_create_table.pl
@@ -0,0 +1,113 @@
+#!/usr/bin/perl -w
+
+use DBI;
+
+# ------------------------------------------------------------------------------
+# kcor_sgs_create_table.pl
+# ------------------------------------------------------------------------------
+# Create MLSO db table: kcor_sgs (mysql).
+# ------------------------------------------------------------------------------
+# Don Kolinski Jan 2017
+#	Added new argument containing path/configfile:
+#		config file format:
+#		username = <value>
+#		password = <value>
+#		host = <value>
+#		dbname = <value>
+#	Added/edited database fields
+# ------------------------------------------------------------------------------
+
+# Check the arguments for existence of config file
+if ($#ARGV != 0 ) {
+    print "Usage: $0 <ConfigFile>\n";
+    exit;
+}
+
+# Warn user of database drop
+print "WARNING!!!! This script will drop the table kcor_img!\nDo you wish to continue? ";
+print "Press <Enter> to continue, or 'q' to quit: ";
+my $input = <STDIN>;
+exit if $input eq "q\n";
+
+# Read config file
+$configfile = $ARGV[0];
+open (CONFIG, "$configfile") or die "ERROR: Config file not found : $configfile";
+while (<CONFIG>) {
+    chomp;                  # no newline
+    s/#.*//;                # no comments
+    s/^\s+//;               # no leading white
+    s/\s+$//;               # no trailing white
+    next unless length;     # anything left?
+    my ($var, $value) = split(/\s*=\s*/, $_, 2);
+    $configvar{$var} = $value;
+} 
+$user = $configvar{"username"};
+$pass = $configvar{"password"};
+$host = $configvar{"host"};
+$db = $configvar{"dbname"};
+
+#---------------------
+# Connect to database.
+#---------------------
+
+$dbh = DBI->connect ("DBI:mysql:$db:$host", $user, $pass) ;
+
+if (! $dbh)
+  {
+  print "DB connection failed.\n" ;
+  die () ;
+  }
+else
+  {
+  print "DB connection successful.\n" ;
+  }
+
+#-------------------------------
+# Create new kcor_sgs table.
+#-------------------------------
+
+$command = "DROP TABLE IF EXISTS kcor_sgs_test" ;
+$sth     = $dbh->prepare ($command) ;
+
+$sth->execute () ;
+if (! $sth)
+  {
+  print "$command\n" ;
+  print "mysql error: $dbh->errstr\n" ;
+  die () ;
+  }
+
+# Define fields
+#	Notes:
+#	Took out 'file_name'.  The data are in the K-Cor files, so we could add it back later.
+#
+$command = "CREATE TABLE kcor_sgs_test
+  (
+  sgs_id    INT (10) AUTO_INCREMENT PRIMARY KEY,
+  date_obs  DATETIME NOT NULL,
+  sgsdimv   FLOAT (7, 4),
+  sgsdims   FLOAT (8, 5),
+  sgssumv   FLOAT (7, 4),
+  sgsrav    FLOAT (9, 6),
+  sgsras    FLOAT (9, 6),
+  sgsrazr   FLOAT (7, 2),
+  sgsdecv   FLOAT (10, 7),
+  sgsdecs   FLOAT (10, 7),
+  sgsdeczr  FLOAT (7, 2),
+  sgsscint  FLOAT (7, 3),
+  sgssums   FLOAT (9, 6)
+  )" ;  # TODO: remove _test when in production
+
+$sth = $dbh->prepare ($command) ;
+$sth->execute () ;
+if (! $sth)
+  {
+  print "$command\n" ;
+  print "mysql error: $dbh->errstr\n" ;
+  die () ;
+  }
+
+#----------------------------------------
+# Terminate connection to mysql database.
+#----------------------------------------
+$dbh->disconnect ;
