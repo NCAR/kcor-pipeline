d2c79454f5b3f8b96789f8d8d7ec9509776ddab6
kolinski <kolinski@ucar.edu>
2017-01-23 11:01:43 -0700

    Edited kcor_hw_create_table.pl to add/edit fields

--- bin/kcor_hw_create_table.pl
+++ bin/kcor_hw_create_table.pl
@@ -8,16 +8,44 @@ use DBI;
 # Create MLSO db table: kcor_hw (mysql).
 # ------------------------------------------------------------------------------
 # Andrew Stanger   MLSO/HAO/NCAR   08 Dec 2015
+# New edits by Don Kolinski Jan 2017
+#	Added new argument containing path/configfile:
+#		config file format:
+#		username = <value>
+#		password = <value>
+#		host = <value>
+#		dbname = <value>
+#	Added/edited database fields
 # ------------------------------------------------------------------------------
-#--- DB name
-#--- DB host
-#--- DB user
-#--- DB password
 
-$db   = "MLSO" ;
-$host = "databases.hao.ucar.edu" ;
-$user = "stanger" ;
-$pass = "mml4so14" ;
+# Check the arguments for existence of config file
+if ($#ARGV != 0 ) {
+    print "Usage: $0 <ConfigFile>\n";
+    exit;
+}
+
+# Warn user of database drop
+print "WARNING!!!! This script will drop the table kcor_hw!\nDo you wish to continue? ";
+print "Press <Enter> to continue, or 'q' to quit: ";
+my $input = <STDIN>;
+exit if $input eq "q\n";
+
+# Read config file
+$configfile = $ARGV[0];
+open (CONFIG, "$configfile") or die "ERROR: Config file not found : $configfile";
+while (<CONFIG>) {
+    chomp;                  # no newline
+    s/#.*//;                # no comments
+    s/^\s+//;               # no leading white
+    s/\s+$//;               # no trailing white
+    next unless length;     # anything left?
+    my ($var, $value) = split(/\s*=\s*/, $_, 2);
+    $configvar{$var} = $value;
+} 
+$user = $configvar{"username"};
+$pass = $configvar{"password"};
+$host = $configvar{"host"};
+$db = $configvar{"dbname"};
 
 #---------------------
 # Connect to database.
@@ -39,7 +67,7 @@ else
 # Create new kcor_hw table.
 #-----------------------------
 
-$command = "DROP TABLE kcor_hw" ;
+$command = "DROP TABLE IF EXISTS kcor_hw_test" ;
 $sth     = $dbh->prepare ($command) ;
 
 $sth->execute () ;
@@ -50,21 +78,23 @@ if (! $sth)
   die () ;
   }
 
-$command = "CREATE TABLE kcor_hw 
+# Define fields
+$command = "CREATE TABLE kcor_hw_test 
   (
   hw_id    INT (10) AUTO_INCREMENT PRIMARY KEY,
-  date      DATETIME NOT NULL,
-  diffsrid  VARCHAR (8),
-  rcamid    VARCHAR (18),
-  tcamid    VARCHAR (18),
-  rcamlut   VARCHAR (14),
-  tcamlut   VARCHAR (14),
-  modltrid  VARCHAR (1),
-  o1id      VARCHAR (4),
-  occltrid  VARCHAR (9),
-  filterid  VARCHAR (12),
-  sgsloop   TINYINT (1)
-  )" ;
+  date      	DATETIME NOT NULL,
+  diffsrid  	CHAR (8),
+  bopal			FLOAT (11, 7),
+  rcamid    	CHAR (18),
+  tcamid    	CHAR (18),
+  rcamlut   	CHAR (14),
+  tcamlut   	CHAR (14),
+  modltrid  	CHAR (1),
+  o1id      	CHAR (14),
+  occltrid  	CHAR (10),
+  filterid  	CHAR (12),
+  calpolid		CHAR (12)
+  )" ;  # TODO: remove _test when in production
 
 $sth = $dbh->prepare ($command) ;
 $sth->execute () ;
