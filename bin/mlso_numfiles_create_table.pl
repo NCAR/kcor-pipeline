1adf4f323dc5010d01748cc2404dad193fd310d5
Michael Galloy <mgalloy@gmail.com>
2017-04-12 16:18:05 -0600

    Add routine to create mlso_numfiles

---
+++ bin/mlso_numfiles_create_table.pl
@@ -0,0 +1,105 @@
+#!/usr/bin/perl -w
+
+use DBI;
+
+# ------------------------------------------------------------------------------
+# mlso_numfiles_create_table.pl
+# ------------------------------------------------------------------------------
+# Create MLSO db table: mlso_numfiles (mysql).
+# ------------------------------------------------------------------------------
+# Don Kolinski April 2017
+#	Added new argument containing path/configfile:
+#		config file format:
+#		username = <value>
+#		password = <value>
+#		host = <value>
+#		dbname = <value>
+#	Added/edited database fields
+# ------------------------------------------------------------------------------
+
+# Check the arguments for existence of config file
+if ($#ARGV != 0 ) {
+    print "Usage: $0 <ConfigFile>\n";
+    exit;
+}
+
+# Warn user of database drop
+print "WARNING!!!! This script will drop the table mlso_numfiles!\nDo you wish to continue? ";
+print "Press <Enter> to continue, or 'q' to quit: ";
+my $input = <STDIN>;
+exit if $input eq "q\n";
+
+# Read config file
+$configfile = $ARGV[0];
+open (CONFIG, "$configfile") or die "ERROR: Config file not found : $configfile";
+while (<CONFIG>) {
+    chomp;                  # no newline
+    s/#.*//;                # no comments
+    s/^\s+//;               # no leading white
+    s/\s+$//;               # no trailing white
+    next unless length;     # anything left?
+    my ($var, $value) = split(/\s*=\s*/, $_, 2);
+    $configvar{$var} = $value;
+} 
+$user = $configvar{"username"};
+$pass = $configvar{"password"};
+$host = $configvar{"host"};
+$db = $configvar{"dbname"};
+
+#---------------------
+# Connect to database.
+#---------------------
+
+$dbh = DBI->connect ("DBI:mysql:$db:$host", $user, $pass) ;
+
+if (! $dbh)
+  {
+  print "DB connection failed.\n" ;
+  die () ;
+  }
+else
+  {
+  print "DB connection successful.\n" ;
+  }
+
+#-------------------------------
+# Create new mlso_numfiles table.
+#-------------------------------
+
+$command = "DROP TABLE IF EXISTS mlso_numfiles" ;
+$sth     = $dbh->prepare ($command) ;
+
+$sth->execute () ;
+if (! $sth)
+  {
+  print "$command\n" ;
+  print "mysql error: $dbh->errstr\n" ;
+  die () ;
+  }
+
+# Define fields
+$command = "CREATE TABLE mlso_numfiles
+  (
+  day_id                   MEDIUMINT (5) AUTO_INCREMENT PRIMARY KEY,
+  obs_day                  DATE NOT NULL,
+  num_kcor_pb_fits         INT(10),
+  num_kcor_nrgf_fits       INT(10),
+  num_kcor_pb_lowresgif    INT(10),
+  num_kcor_pb_fullresgif   INT(10),
+  num_kcor_nrgf_lowresgif  INT(10),
+  num_kcor_nrgf_fullresgif INT(10)
+  )" ; 
+
+$sth = $dbh->prepare ($command) ;
+$sth->execute () ;
+if (! $sth)
+  {
+  print "$command\n" ;
+  print "mysql error: $dbh->errstr\n" ;
+  die () ;
+  }
+
+#----------------------------------------
+# Terminate connection to mysql database.
+#----------------------------------------
+$dbh->disconnect ;
