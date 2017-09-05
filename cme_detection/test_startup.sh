d1609eb4b948140dcdca0c0e09b70cbf0c508894
Michael Galloy <mgalloy@gmail.com>
2017-09-05 13:05:24 -0600

    Install CME detection

---
+++ cme_detection/test_startup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export KCOR_DIR=/export/data1/Data/KCor/archive
+export KCOR_HPR_DIR=/export/data1/Data/KCor/hpr
+export KCOR_HPR_DIFF_DIR=/export/data1/Data/KCor/hpr_diff
+export KCOR_MOVIE_DIR=/export/data1/Data/KCor/cme_movies
+
+IDL=/usr/local/bin/idl
+
+KCOR_CME_ROOT=$(dirname $0)
+KCOR_CME_PATH=+${KCOR_CME_ROOT}:"<IDL_DEFAULT>"
+
+export KCOR_MAILING_LIST=$KCOR_CME_ROOT/test-mailing-list
+
+${IDL} -IDL_PATH ${KCOR_CME_PATH} -IDL_STARTUP '' -e "kcor_cme_detection, '2017-08-20'"
