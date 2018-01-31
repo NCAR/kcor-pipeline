3b169b68392bbfe76a3ea9dcf42a7008f51c9a86
Michael Galloy <mgalloy@gmail.com>
2018-01-31 10:39:50 -0700

    Script that will be run for automated, real-time CME detection for #50

---
+++ cme_detection/kcor_realtime_cme.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+canonicalpath() {
+  if [ -d $1 ]; then
+    pushd $1 > /dev/null 2>&1
+    echo $PWD
+  elif [ -f $1 ]; then
+    pushd $(dirname $1) > /dev/null 2>&1
+    echo $PWD/$(basename $1)
+  else
+    echo "Invalid path $1"
+  fi
+  popd > /dev/null 2>&1
+}
+
+IDL=/home/mgalloy/bin/idl
+
+SCRIPT_LOC=$(canonicalpath $0)
+KCOR_CME_ROOT=$(dirname $SCRIPT_LOC)
+KCOR_ROOT=$(dirname $KCOR_CME_ROOT)
+KCOR_CME_PATH=+${KCOR_CME_ROOT}:+${KCOR_ROOT}/lib:+${KCOR_ROOT}/src:"<IDL_DEFAULT>"
+SHORT_HOSTNAME=$(hostname | sed -e 's/\..*$//')
+
+# TODO: change to cme when put into production
+FLAGS=cmetest
+
+CONFIG_FILENAME=$KCOR_ROOT/config/kcor.$(whoami).$SHORT_HOSTNAME.$FLAGS.cfg
+
+idl_cmd="kcor_cme_detection_job, config_filename='$CONFIG_FILENAME', /realtime"
+${IDL} -IDL_PATH ${KCOR_CME_PATH} -IDL_STARTUP '' -e "${idl_cmd}"
