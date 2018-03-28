3fa0dcc64a9b16b62d90738831d5fe4bc329a0a7
Michael Galloy <mgalloy@gmail.com>
2018-03-28 16:52:37 -0600

    Add script to check that automated, real-time CME detection is running
    Will restart and notify if not running

---
+++ cme_detection/check_cme_det_realtime.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Checks the automated, real-time CME detection process and restarts it if it
+# is not running.
+
+pid=$(pgrep -u mgalloy -f 'kcor_cme_detection_job.*/realtime')
+if [ $? -eq 1 ]; do
+  # status=1 if automated, real-time CME detection is not running
+
+  # restart process
+  script=$(dirname $0)/kcor_realtime_cme.sh
+  $script
+
+  # send notification that process wasn't running and restarting it
+  body="Restarted $script\n\nSent from $0 ($(whoami)@$(hostname))"
+  addresses=mgalloy@ucar.edu
+  echo -e $body | mail -s "Restarting automated, real-time CME detection process" \
+                    -r $(whoami)@ucar.edu \
+                    $addresses
+fi
