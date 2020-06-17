2a6c1419a23180494bcea21ab2195db9a14fa5a9
Michael Galloy <mgalloy@gmail.com>
2020-06-17 10:45:31 -0600

    Updating validation files

--- linux_configure.sh
+++ linux_configure.sh
@@ -4,9 +4,12 @@ rm -rf build
 mkdir build
 cd build
 
+IDL_ROOT_DIR=/opt/share/idl8.6/idl86
+#IDL_ROOT_DIR=/opt/share/idl8.7.3/idl87
+
 ~mgalloy/software/bin/cmake \
   -DCMAKE_INSTALL_PREFIX:PATH=~/software/kcor-pipeline \
-  -DIDL_ROOT_DIR:PATH=/opt/share/idl8.7.3/idl87 \
+  -DIDL_ROOT_DIR:PATH=${IDL_ROOT_DIR} \
   -DIDLdoc_DIR:PATH=~/projects/idldoc \
   -Dmgunit_DIR:PATH=~/projects/mgunit/src \
   ..
