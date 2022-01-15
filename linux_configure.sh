5509f6946ffbf738cee426e9c62e97cc0a5beccf
Michael Galloy <mgalloy@gmail.com>
2022-01-14 14:10:06 -1000

    Adding a comment line for running on kodiak

--- linux_configure.sh
+++ linux_configure.sh
@@ -7,6 +7,9 @@ cd build
 #IDL_ROOT_DIR=/opt/share/idl8.8/idl88
 IDL_ROOT_DIR=/opt/share/idl8.7.3/idl87
 
+# on kodiak
+#IDL_ROOT_DIR=/usr/local/idl873/idl87
+
 ~mgalloy/software/bin/cmake \
   -DCMAKE_INSTALL_PREFIX:PATH=~/software/kcor-pipeline \
   -DIDL_ROOT_DIR:PATH=${IDL_ROOT_DIR} \
