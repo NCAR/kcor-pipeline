6a51ac9d6738893cde12d645e6a208e055949f42
Michael Galloy <mgalloy@gmail.com>
2022-07-12 15:28:56 -0600

    Work on the KCor CME event database table

--- bin/archive_cme_alerts.sh
+++ bin/archive_cme_alerts.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+SCRIPT_DIR=$(dirname $0)
+
 REMOTE_SERVER=$1
 REMOTE_PATH=/export/data1/Data/KCor/raw
 if [[ $# -lt 2 ]]; then
@@ -18,8 +20,21 @@ SSH_KEY=${HOME}/.ssh/id_rsa2
 
 # create directory to copy to and copy into it
 mkdir -p ${LOCAL_PATH}/${DATE}
-cmd="scp -rq -i ${SSH_KEY} ${REMOTE_SERVER}.mlso.ucar.edu:${REMOTE_PATH}/${DATE}/p/*cme* ${LOCAL_PATH}/${DATE}"
+cmd="scp -rqp -i ${SSH_KEY} ${REMOTE_SERVER}.mlso.ucar.edu:${REMOTE_PATH}/${DATE}/p/*cme* ${LOCAL_PATH}/${DATE}"
 $cmd
+
+
+REMOTE_PATH=/export/data1/Data/KCor/cme-alerts
+YEAR=${DATE:0:4}
+MONTH=${DATE:4:2}
+DAY=${DATE:6:2}
+
+# copy all files of the following form to the LOCAL_PATH:
+#   cme-alerts/YYYY/MM/DD/mlso_kcor.2017-10-06T172637Z.2017-10-06T173105Z.json
+cmd="scp -qp -i ${SSH_KEY} ${REMOTE_SERVER}.mlso.ucar.edu:${REMOTE_PATH}/${YEAR}/${MONTH}/${DAY}/*.json ${LOCAL_PATH}/${DATE}"
+$cmd
+
+${SCRIPT_DIR}/kcor_add_events ${LOCAL_PATH}/${DATE}
 
 # remove directory if nothing was copied
 find ${LOCAL_PATH} -maxdepth 1 -empty -exec rmdir {} \;
---
+++ bin/kcor_event_create_table.pl
@@ -0,0 +1,110 @@
+#!/usr/bin/perl -w
+
+use DBI;
+
+# ------------------------------------------------------------------------------
+# kcor_event_create_table.pl
+# ------------------------------------------------------------------------------
+# Create MLSO db table: kcor_event (mysql).
+# ------------------------------------------------------------------------------
+# Andrew Stanger   MLSO/HAO/NCAR   08 Dec 2015
+# New edits by Don Kolinski Jan 2017
+# Added new argument containing path/configfile:
+#   config file format:
+#   username = <value>
+#   password = <value>
+#   host = <value>
+#   dbname = <value>
+# Added/edited database fields
+# ------------------------------------------------------------------------------
+
+# Check the arguments for existence of config file
+if ($#ARGV != 0 ) {
+    print "Usage: $0 <ConfigFile>\n";
+    exit;
+}
+
+# Warn user of database drop
+print "WARNING!!!! This script will drop the table kcor_raw!\nDo you wish to continue? ";
+print "Press <Enter> to continue, or 'q' to quit: ";
+my $input = <STDIN>;
+exit if $input eq "q\n";
+
+# Read config file
+$configfile = $ARGV[0];
+open (CONFIG, "$configfile") or die "ERROR: Config file not found : $configfile";
+while (<CONFIG>) {
+    chomp;                  # no newline
+    s/#.*//;                # no comments
+    s/^\s+//;               # no leading white
+    s/\s+$//;               # no trailing white
+    next unless length;     # anything left?
+    my ($var, $value) = split(/\s*=\s*/, $_, 2);
+    $configvar{$var} = $value;
+} 
+$user = $configvar{"username"};
+$pass = $configvar{"password"};
+$host = $configvar{"host"};
+$db = $configvar{"dbname"};
+
+#---------------------
+# Connect to database.
+#---------------------
+
+$dbh = DBI->connect ("DBI:mysql:$db:$host", $user, $pass) ;
+
+if (! $dbh)
+  {
+  print "DB connection failed.\n" ;
+  die () ;
+  }
+else
+  {
+  print "DB connection successful.\n" ;
+  }
+
+#-------------------------------
+# Create new kcor_raw table.
+#-------------------------------
+
+$command = "DROP TABLE IF EXISTS kcor_event" ;
+$sth     = $dbh->prepare ($command) ;
+
+$sth->execute () ;
+if (! $sth)
+  {
+  print "$command\n" ;
+  print "mysql error: $dbh->errstr\n" ;
+  die () ;
+  }
+
+# Define fields
+$command = "create table kcor_event(
+  event_id         int (10) auto_increment primary key, 
+  obs_day          mediumint (5) not null,
+  instrument       char(10),
+  start_time       datetime not null,
+  end_tme          datetime,
+  position_angle   float,
+  speed            float,
+  height           float,
+  time             datetime not null,
+  all_clear        boolean,
+  comment          varchar(100),
+  index(date_obs),
+  foreign key (obs_day) references mlso_numfiles(day_id)
+)";
+
+$sth = $dbh->prepare ($command) ;
+$sth->execute () ;
+if (! $sth)
+  {
+  print "$command\n" ;
+  print "mysql error: $dbh->errstr\n" ;
+  die () ;
+  }
+
+#----------------------------------------
+# Terminate connection to mysql database.
+#----------------------------------------
+$dbh->disconnect ;
