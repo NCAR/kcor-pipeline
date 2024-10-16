3456c9264fc26d7139ce4aa4d382b584aa8818b9
Michael Galloy <mgalloy@gmail.com>
2024-10-16 21:58:44 +0200

    Update Mac configuration for arm64

--- mac_configure.sh
+++ mac_configure.sh
@@ -3,9 +3,12 @@
 rm -rf build
 mkdir build
 cd build
+#IDL_ROOT_DIR=/Applications/harris/idl89
+IDL_ROOT_DIR=/Applications/NV5/idl90
 
 cmake \
   -DCMAKE_INSTALL_PREFIX:PATH=~/software/kcor-pipeline \
+  -DIDL_ROOT_DIR:PATH=${IDL_ROOT_DIR} \
   -DIDLdoc_DIR:PATH=~/projects/idldoc \
   -Dmgunit_DIR:PATH=~/projects/mgunit/src \
   ..
