f31ae254dc88e6990e3171ec8a358d1b0a6768f1
Michael Galloy <mgalloy@gmail.com>
2024-11-19 10:12:00 -0700

    Create kcor_performance database table

---
+++ bin/kcor_performance_create_table.pl
@@ -0,0 +1,111 @@
+#!/usr/bin/perl -w
+
+use DBI;
+
+# ------------------------------------------------------------------------------
+# kcor_img_create_table.pl
+# ------------------------------------------------------------------------------
+# Create MLSO db table: kcor_img (mysql).
+# ------------------------------------------------------------------------------
+# Andrew Stanger   MLSO/HAO/NCAR   08 Dec 2015
+# New edits by Don Kolinski Jan 2017
+#	Added new argument containing path/configfile:
+#		config file format:
+#		username = <value>
+#		password = <value>
+#		host = <value>
+#		dbname = <value>
+#	Added/edited database fields
+# ------------------------------------------------------------------------------
+
+# Check the arguments for existence of config file
+if ($#ARGV != 0 ) {
+    print "Usage: $0 <ConfigFile>\n";
+    exit;
+}
+
+# Warn user of database drop
+print "WARNING!!!! This script will drop the table kcor_img!\nDo you wish to continue? ";
+print "Press <Enter> to continue, or 'q' to quit: ";
+my $input = <STDIN>;
+exit if $input eq "q\n";
+
+# Read config file
+$configfile = $ARGV[0];
+open (CONFIG, "$configfile") or die "ERROR: Config file not found : $configfile";
+while (<CONFIG>) {
+    chomp;                  # no newline
+    s/#.*//;                # no comments
+    s/^\s+//;               # no leading white
+    s/\s+$//;               # no trailing white
+    next unless length;     # anything left?
+    my ($var, $value) = split(/\s*=\s*/, $_, 2);
+    $configvar{$var} = $value;
+} 
+$user = $configvar{"username"};
+$pass = $configvar{"password"};
+$host = $configvar{"host"};
+$db = $configvar{"dbname"};
+
+#---------------------
+# Connect to database.
+#---------------------
+
+$dbh = DBI->connect ("DBI:mysql:$db:$host", $user, $pass) ;
+
+if (! $dbh)
+  {
+  print "DB connection failed.\n" ;
+  die () ;
+  }
+else
+  {
+  print "DB connection successful.\n" ;
+  }
+
+#----------------------------------
+# Create new kcor_performance table.
+#-----------------------------------
+
+$command = "drop table if exists kcor_performance" ;
+$sth     = $dbh->prepare ($command) ;
+
+$sth->execute () ;
+if (! $sth)
+  {
+  print "$command\n" ;
+  print "mysql error: $dbh->errstr\n" ;
+  die () ;
+  }
+
+$command = "create table kcor_performance (
+  performance_id        int (10) auto_increment primary key,
+  dt_created            timestamp default current_timestamp,
+  obs_day               mediumint (5) not null,
+  n_l0_files            int,
+  n_l2_files            int,
+  rt_start              timestamp,
+  rt_end                timestamp,
+  rt_duration           float,
+  eod_start             timestamp,
+  eod_end               timestamp,
+  eod_duration          float,
+  kcor_sw_id            int(10),
+  index (obs_day),
+  foreign key (kcor_sw_id) references kcor_sw(sw_id),
+  foreign key (obs_day) references mlso_numfiles(day_id)
+  )" ;
+
+$sth = $dbh->prepare ($command) ;
+$sth->execute () ;
+if (! $sth)
+  {
+  print "$command\n" ;
+  print "mysql error: $dbh->errstr\n" ;
+  die () ;
+  }
+
+#----------------------------------------
+# Terminate connection to mysql database.
+#----------------------------------------
+$dbh->disconnect ;
